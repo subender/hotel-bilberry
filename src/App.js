576c88b5867c595a0250aac4a1bcfbbb15873e90
subender <subender.io@outlook.com>
2023-02-10 08:52:55 +0530

    added react routers and context provider.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import RootLayout from "./components/pages/Root";
+import CreateContainer from "./components/CreateContainer";
+import { AnimatePresence } from "framer-motion";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <MainContainer /> },
+      { path: "createItem", element: <CreateContainer /> },
+    ],
+  },
+]);
 function App() {
-  return <div className=" text-red-400">working?</div>;
+  return (
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <AnimatePresence>
+        <RouterProvider router={router}></RouterProvider>;
+      </AnimatePresence>
+    </StateProvider>
+  );
 }
 
 export default App;
