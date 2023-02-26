def60cf56b1ba6f96249d50bcef8771b266cd35f
subender <subender.io@outlook.com>
2023-02-27 02:01:12 +0530

    added data for hero section.

---
+++ src/Utils/HeroData.js
@@ -0,0 +1,73 @@
+import I1 from "../img/i1.png";
+import R1 from "../img/r1.png";
+import FI1 from "../img/fi1.png";
+import R5 from "../img/r5.png";
+export const heroData = [
+  {
+    id: 1,
+    name: "Icecream",
+    decp: "Chocolate & Berry",
+    price: "9.98",
+    imgScr: I1,
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    decp: "Farm Fresh",
+    price: "3.99",
+    imgScr: R1,
+  },
+  {
+    id: 3,
+    name: "Veg Fish",
+    decp: "Vegan Fish",
+    price: "9.99",
+    imgScr: FI1,
+  },
+  {
+    id: 4,
+    name: "Veg Pulao",
+    decp: "Veggies & Paneer",
+    price: "9.98",
+    imgScr: R5,
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
