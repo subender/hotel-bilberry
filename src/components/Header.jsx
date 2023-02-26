ba4c3a3d0a7168cbdb8c0ab3d169f3b13099135d
subender <subender.io@outlook.com>
2023-02-27 01:57:51 +0530

    added mobile navigation.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,6 +32,8 @@ const Header = () => {
     } else {
       setIsMenu(!isMenu);
     }
+
+    console.log(user);
   };
 
   const logoutHandler = () => {
@@ -45,9 +47,9 @@ const Header = () => {
   };
 
   return (
-    <header className=" fixed z-40 w-screen p-3 px-4 md:p-6 md:px-16">
+    <header className=" fixed z-40 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* Desktop & Tabs*/}
-      <nav className="hidden md:flex h-full items-center justify-between">
+      <nav className="hidden md:flex h-full items-center justify-between ">
         <Link to="/" className=" flex items-center gap-2 ">
           <img className=" w-8 object-cover" src={logo} alt="BilBerry's logo" />
           <p className=" text-headingColor font-bold text-xl">Bil-Berry</p>
@@ -95,7 +97,10 @@ const Header = () => {
               >
                 {user && user.email === "subendernath@gmail.com" && (
                   <Link to="createItem">
-                    <p className="flex items-center gap-3 px-4 py-2 hover:bg-slate-200 transition-all text-textColor text-base duration-100 ease-out cursor-pointer rounded-lg">
+                    <p
+                      className="flex items-center gap-3 px-4 py-2 hover:bg-slate-200 transition-all text-textColor text-base duration-100 ease-out cursor-pointer rounded-lg"
+                      onClick={() => setIsMenu(false)}
+                    >
                       New Item
                       <MdAdd />
                     </p>
@@ -115,6 +120,16 @@ const Header = () => {
       </nav>
       {/* Mobile*/}
       <div className=" flex md:hidden h-full justify-between">
+        <motion.div
+          whileTap={{ scale: 1.2 }}
+          className=" flex justify-center items-center relative"
+        >
+          <MdShoppingBasket className=" text-textColor text-2xl cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out" />
+          <div className=" absolute -top-1 -right-2 h-5 w-5 bg-cartNumBg rounded-full flex items-center justify-center">
+            <p className=" text-xs text-white font-semibold">2</p>
+          </div>
+        </motion.div>
+
         <Link to="/" className=" flex items-center gap-2 ">
           <img className=" w-8 object-cover" src={logo} alt="BilBerry's logo" />
           <p className=" text-headingColor font-bold text-xl">Bil-Berry</p>
@@ -138,7 +153,10 @@ const Header = () => {
             >
               {user && user.email === "subendernath@gmail.com" && (
                 <Link to="createItem">
-                  <p className="flex items-center gap-3 px-4 py-2 hover:bg-slate-200 transition-all text-textColor text-base duration-100 ease-out cursor-pointer rounded-lg">
+                  <p
+                    className="flex items-center gap-3 px-4 py-2 hover:bg-slate-200 transition-all text-textColor text-base duration-100 ease-out cursor-pointer rounded-lg"
+                    onClick={() => setIsMenu(false)}
+                  >
                     New Item
                     <MdAdd />
                   </p>
@@ -146,16 +164,30 @@ const Header = () => {
               )}
 
               <ul className="flex flex-col   gap-1 ml-auto">
-                <li className=" text-base cursor-pointer px-4 py-2 text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg">
-                  Home
-                </li>
-                <li className="text-base px-4 py-2 cursor-pointer text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg">
+                <Link to="/">
+                  <li
+                    className=" text-base cursor-pointer px-4 py-2 text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg"
+                    onClick={() => setIsMenu(false)}
+                  >
+                    Home
+                  </li>
+                </Link>
+                <li
+                  className="text-base px-4 py-2 cursor-pointer text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg"
+                  onClick={() => setIsMenu(false)}
+                >
                   Menu
                 </li>
-                <li className="px-4 py-2 text-base cursor-pointer text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg">
+                <li
+                  className="px-4 py-2 text-base cursor-pointer text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg"
+                  onClick={() => setIsMenu(false)}
+                >
                   About Us
                 </li>
-                <li className="px-4 py-2 text-base cursor-pointer text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg">
+                <li
+                  className="px-4 py-2 text-base cursor-pointer text-textColor hover:bg-slate-200 transition-all  hover:text-textColor hover:text-base duration-100 ease-out rounded-lg"
+                  onClick={() => setIsMenu(false)}
+                >
                   Service
                 </li>
               </ul>
