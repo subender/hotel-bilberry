957e6944ed98875e9b2e11add30d4853aec64710
subender <subender.io@outlook.com>
2023-02-27 01:55:37 +0530

    added form feature uptill image upload feature.

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,218 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  MdFastfood,
+  MdCloudUpload,
+  MdDelete,
+  MdFoodBank,
+  MdAttachMoney,
+} from "react-icons/md";
+import { categories } from "../Utils/HeroData";
+import Loader from "./Loader";
+import { storage } from "../Firebase.config";
+import { upload } from "@testing-library/user-event/dist/upload";
 
 const CreateContainer = () => {
-  return <div>CreateContainer</div>;
+  const [title, setTitle] = useState("");
+  const [calories, setCalories] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [fields, setFields] = useState(false);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const uploadImage = (e) => {
+    setIsLoading(true);
+    const uploadedFile = e.target.files[0];
+
+    // Create a reference to the Firebase storage with a unique file name
+    const storageRef = ref(
+      storage,
+      `images/${Date.now()}-${uploadedFile.name}`
+    );
+
+    // Create an upload task using the storage reference and uploaded file
+    const uploadTask = uploadBytesResumable(storageRef, uploadedFile);
+
+    // Listen for the state of the upload task
+    uploadTask.on(
+      "state_changed", // If the state changes, update the upload progress
+      (snapshot) => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      },
+
+      // If there is an error, set state variables and display an error message
+      (error) => {
+        console.log(error);
+        setFields(true);
+        setMsg("Error while uploading the image : Please try again 🙏");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      },
+
+      // If the upload is successful, get the download URL and set state variables accordingly
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downlodURL) => {
+          setImageAsset(downlodURL);
+          setIsLoading(false);
+          setFields(true);
+          setMsg("Image Uploaded Sucessfully !!");
+          setAlertStatus("success");
+          setTimeout(() => {
+            setFields(false);
+          }, 4000);
+        });
+      }
+    );
+  };
+
+  const deleteImage = () => {};
+
+  const saveDetails = () => {};
+
+  return (
+    <div className="w-full h-auto min-h-screen flex items-center justify-center ">
+      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex justify-center items-center flex-col gap-4">
+        {fields && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={`w-full p-2 rounded-lg text-center text-lg font-semibold 
+        
+        ${
+          alertStatus === "danger"
+            ? "bg-red-400 text-red-800"
+            : "bg-emerald-400 text-emerald-800"
+        }`}
+          >
+            {msg}
+          </motion.p>
+        )}
+
+        <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2 ">
+          <MdFastfood className="text-xl text-gray-700" />
+          <input
+            type="text"
+            required
+            value={title}
+            placeholder="Your Title..."
+            className="w-full h-full bg-transparent  outline-none border-none placeholder:text-gray-20000 text-textColor"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="w-full">
+          <select
+            onChange={(e) => setCategory(e.target.value)}
+            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+          >
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+            {categories &&
+              categories.map((cat) => (
+                <option
+                  key={cat.id}
+                  className="text-base outline-none border-0 capitalize text-headingColor bg-white"
+                  value={cat.urlParamName}
+                >
+                  {cat.name}
+                </option>
+              ))}
+          </select>
+        </div>
+
+        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center">
+                      <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700 " />
+                      <p className="text-gray-500 hover:text-gray-700">
+                        Click here to upload
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      name="uploadimage"
+                      accept="image/*"
+                      onChange={uploadImage}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className="relative h-full">
+                    <img
+                      src={imageAsset}
+                      alt="uploaded image"
+                      className="w-full h-full object-cover"
+                    />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out
+                    "
+                      onClick={deleteImage}
+                    >
+                      <MdDelete className="text-white" />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>
+
+        <div className="w-full flex flex-col md:flex-row items-center gap-3">
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2 ">
+            <MdFoodBank className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+              placeholder="Calories.."
+              className="bg-transparent w-full text-lg outline-none border-none placeholder:text-gray-400 text-textColor"
+            />
+          </div>
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2 ">
+            <MdAttachMoney className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              placeholder="Price"
+              className="bg-transparent w-full text-lg outline-none border-none placeholder:text-gray-400 text-textColor"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center w-full ">
+          <button
+            type="button"
+            className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold hover:bg-emerald-600 transition-all duration-200 ease-in-out"
+            onClick={saveDetails}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateContainer;
