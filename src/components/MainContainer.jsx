ca62055d9b219960e3e98f592c37977c3d795a6f
subender <subender.io@outlook.com>
2023-02-27 02:00:48 +0530

    quick fixes.

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HomeContainer from "./HomeContainer";
 
 const MainContainer = () => {
-  return <div>MainContainer</div>;
+  return (
+    <div className="flex w-full h-auto items-center justify-center">
+      <HomeContainer />
+    </div>
+  );
 };
 
 export default MainContainer;
