998185d8061b7bda435822e57a04286a1de9aeb5
subender <subender.io@outlook.com>
2023-03-01 04:23:11 +0530

    added getFoodItems function.

--- src/Utils/firebasefunctions.js
+++ src/Utils/firebasefunctions.js
@@ -1,8 +1,25 @@
-import { doc, setDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  setDoc,
+  query,
+} from "firebase/firestore";
 import { firestore } from "../Firebase.config";
 
 export const saveItem = async (data) => {
   await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
     merge: true,
   });
+};
+
+export const getFoodItems = async () => {
+  const items = await getDocs(
+    query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+  );
+
+  return items.docs.map((doc) => doc.data());
 };
