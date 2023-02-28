afe916ff62250e1c4eb586c496f9396272452096
subender <subender.io@outlook.com>
2023-03-01 04:15:46 +0530

    added cards and container for items.

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import HomeContainer from "./HomeContainer";
-
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import RowContainer from "./RowContainer";
+import { useStateValue } from "../context/StateProvider";
 const MainContainer = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+
   return (
-    <div className="flex w-full h-auto items-center justify-center">
+    <div className="flex w-full h-auto flex-col items-center justify-center">
       <HomeContainer />
+
+      <section className="w-full">
+        <div className="w-full flex items-center justify-between">
+          <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600  transition-all ease-in-out duration-100">
+            Our fresh & health fruits
+          </p>
+
+          <div className="hidden md:flex gap-3 items-center ">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex justify-center items-center"
+            >
+              <MdChevronLeft className="text-lg text-white" />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex justify-center items-center"
+            >
+              <MdChevronRight className="text-lg text-white" />
+            </motion.div>
+          </div>
+        </div>
+        <RowContainer
+          flag={true}
+          data={foodItems?.filter((item) => item.category === "fruits")}
+        />
+      </section>
     </div>
   );
 };
