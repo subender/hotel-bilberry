f3f9a73a8ef3c5330c9176a0ce3f809ea66285c2
subender <subender.io@outlook.com>
2023-03-01 04:22:04 +0530

    added function fetching data on load from firebase

--- src/components/pages/Root.js
+++ src/components/pages/Root.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
+import { useStateValue } from "../../context/StateProvider";
+import { useEffect } from "react";
+import { actionType } from "../../context/reducer";
+import { getFoodItems } from "../../Utils/firebasefunctions";
 
 const RootLayout = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+  const fetchData = async () => {
+    await getFoodItems().then((data) => {
+      console.log(data);
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className=" w-screen h-auto flex flex-col bg-primary">
       <Header />
